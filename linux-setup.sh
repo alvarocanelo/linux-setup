dfa07b222dde430d86154e9bf86a0104e4b713dd
acanelo <acanelo@bmat.com>
2017-05-08 00:03:01 +0200

    Write files copy code

--- linux-setup.sh
+++ linux-setup.sh
@@ -1,25 +1,72 @@
 #!/usr/bin/env bash
 
+solve_conflict(){
+    f="$1"
+    if [ -e "/$f" ] ; then
+        # Ask if check differences
+        echo
+        echo "File /$f already exists."
+        while true ; do
+            q="Do you want to check differences for this file? [Y/n]: "
+            read -p "$q" yn
+            case $yn in
+                [Yy]*)
+                    diff "/$f" "$(dirname $0)/$f"
+                    break ;;
+                [Nn]*)
+                    break ;;
+            esac
+        done
+        while true ; do
+            q="Do you want to overwrite it or skip it? [Y/n]: "
+            read -p "$q" yn
+            case $yn in
+                [Yy]*)  break ;;
+                [Nn]*)  return 1 ;;
+            esac
+        done
+    fi
+    return 0
+}
+
 set -e
 
-# Linux packages
+# ### Linux packages
+# # Add repositories signing keys to be able to verify downloaded packages
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+# # Add repositories
+# echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+# # Update list of available packages
+# printf "\nUpdating aptitude:\n"
+# sudo aptitude update
+# # Install packages
+# packages="
+# curl
+# ipython
+# radiotray
+# screen
+# skype
+# spotify.client
+# sshfs
+# terminator
+# tmux
+# tree
+# wget
+# "
+# printf "\nInstalling the following packages:\n\n"
+# for p in $packages ; do echo "$p" ; done ; echo
+# sudo aptitude install -y $packages
 
-printf "\nUpdating aptitude:\n"
-sudo aptitude update
-
-packages="
-curl
-ipython
-radiotray
-screen
-skype
-sshfs
-terminator
-tmux
-tree
-wget
+### Copy configuration files
+directories="
+home
 "
-printf "\nInstalling the following packages:\n\n"
-for p in $packages ; do echo "$p" ; done ; echo
-
-sudo aptitude install -y terminator
+current_dir="$(dirname $0)"
+for d in $directories ; do
+    for f in $(find $d -type f) ; do
+        if solve_conflict "$f" ; then
+            echo "Copying /$f"
+            echo cp "$current_dir"/"$f" /"$f"
+        fi
+    done
+done
